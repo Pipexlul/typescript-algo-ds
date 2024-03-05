5177d936a0c49485daec5364abe24d6fd6deab80
Pipexlul <felipgg@gmail.com>
2024-03-05 15:36:35 -0300

    chore: create first algorithm problem and it's solutions

---
+++ src/algorithms/01-ReverseString.ts
@@ -0,0 +1,47 @@
+import { SimpleLogger } from "../utils/logger";
+
+const disableLogger = false;
+const logger = new SimpleLogger("REVERSE STRING", disableLogger);
+
+/**
+ * @description - Reverse a string using core JavaScript methods
+ * @param str - String to be reversed
+ * @returns - Reversed string
+ */
+const reverseStringMethods = (str: string): string => {
+  return str.split("").reverse().join("");
+};
+
+/**
+ * @description - Reverse a string using a loop
+ * @param str - String to be reversed
+ * @returns - Reversed string
+ */
+const reverseStringLoop = (str: string): string => {
+  // An array is used to prevent concatenating to a string with each iteration
+  // Similar to using StringBuilder(s) in other languages
+  const reversedArr: string[] = [];
+
+  const strLen = str.length; // Save this here to prevent accessing it in the loop every iteration
+
+  for (let i = strLen - 1; i >= 0; --i) {
+    reversedArr.push(str[i]);
+  }
+
+  return reversedArr.join("");
+};
+
+// TODO: FG: Move these to their own testing section later on
+logger.log("METHOD");
+logger.log("Hello There");
+logger.log(reverseStringMethods("Hello There"));
+logger.log("ArriVeDerci");
+logger.log(reverseStringMethods("ArriVeDerci"));
+
+logger.log("LOOP");
+logger.log("Hello There");
+logger.log(reverseStringLoop("Hello There"));
+logger.log("ArriVeDerci");
+logger.log(reverseStringLoop("ArriVeDerci"));
+
+export { reverseStringMethods, reverseStringLoop };
